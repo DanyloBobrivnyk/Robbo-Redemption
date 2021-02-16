2a2d3b3647842755e3bbc20f2bac08211b12c862
daheergt <73279422+daheergt@users.noreply.github.com>
2021-02-16 13:43:52 +0300

    turel
    i finished the randombox and walls

--- 2D_Platfomer_Puzzle/Assets/boxorwall.cs
+++ 2D_Platfomer_Puzzle/Assets/boxorwall.cs
@@ -6,13 +6,43 @@ public class boxorwall : MonoBehaviour
 {
     public int health;
 
+    [System.Serializable]
+    public class Wave
+    {
+         public GameObject prefabs;
+         public int chances;
+    }
+
+    public Wave[] items;
+
+
     public void takedamage()
     {
         health -= 1;
 
-        if(health <= 0)
+        if (health <= 0)
         {
+            if (items != null)
+            {
+                RandomSpawn();
+            }
             Destroy(gameObject);
+        }
+    }
+
+    void RandomSpawn()
+    {
+        int rand = Random.Range(0, 100);
+        int rangeStart = 0;
+        for (int n = 0; n < items.Length; n++)
+        {
+            int rangeEnd = rangeStart + items[n].chances;
+
+            if (rand >= rangeStart && rand < rangeEnd)
+            {
+                Instantiate(items[n].prefabs, transform.position, Quaternion.identity);
+            }
+            rangeStart = rangeEnd;
         }
     }
 }
