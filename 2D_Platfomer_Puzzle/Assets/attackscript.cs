9afc9783d1f3301708629cf3d4e01b78cedd11f0
daheergt <73279422+daheergt@users.noreply.github.com>
2021-02-16 12:25:39 +0300

    turel
    the box and wall

---
+++ 2D_Platfomer_Puzzle/Assets/attackscript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackscript : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    bool facingRight = true;
+
+    public float timeBetweenAttacks;
+    float nextAttackTime;
+
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask boxeslayer;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+
+        if (input > 0 && facingRight == false)
+        {
+            Flip();
+        }
+        else if (input < 0 && facingRight == true)
+        {
+            Flip();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextAttackTime)
+        {
+                anim.SetTrigger("attack");
+                nextAttackTime = Time.time + timeBetweenAttacks;
+        }
+    }
+    void Flip()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        facingRight = !facingRight;
+    }
+    public void Attack()
+    {
+        Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, boxeslayer);
+        foreach (Collider2D col in enemiesToDamage)
+        {
+            if(col.GetComponent<boxorwall>() != null)
+            {
+                col.GetComponent<boxorwall>().takedamage();
+            }
+        }
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
---
+++ 2D_Platfomer_Puzzle/Assets/boxorwall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxorwall : MonoBehaviour
+{
+    public int health;
+
+    public void takedamage()
+    {
+        health -= 1;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
